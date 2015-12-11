1b0aef0b7f08f596a845bb708cd86aad54ae7e3d
Shalliestera <hololovesapples@163.comm>
2015-12-11 15:43:14 +0800

    Start a new one. Prevous one fails.

--- CContacts.h
+++ CContacts.h
@@ -1,41 +1,21 @@
-#ifndef CCONTACT_H_
-#define CCONTACT_H_
-#include <map>
-#include <string>
-#include "CData.h"
+#ifndef CContacts
+#define CContacts
 
 class CContacts {
 private:
-	const int NAMELIM{30};
-	// name + (CData)info
-	std::map<const std::string, const CData> m_contacts;
-public: // public Clear() for testing.
-	void Clear();
+	// map<std::string, std::string>
+	map<std::string, std::string> m_contacts;
 public:
-	// ctor(s)
 	CContacts();
-	CContacts(const string& name);
-	// init
-	void init(const string& name = "nobody");
-	// Add a contact
-	void Add();	// interactive
-	void Add(const string& name, CData& data);
-	// Delete someone
+	CContacts(std::string, std::string);
+	Init();
+	// methods
+	// add del modify search
+	// load / save
+	void Add();
 	void Del();
-	void Del(const string& name);
-	// Modify someone's info
 	void Mod();
-	void Mod(const string& name);
-	// Search someone
-	void Search();
-	bool Search(const string& name);
-	// List all contacts and info
-	void List() const;
-	// Load from a file
+	void Seek();
 	void Load();
-	void LoadFrom(const char * fileName);
-	// Save as a file
 	void Save();
-	void SaveAs(const char * fileName);
 };
-#endif // CCONTACT_H_
